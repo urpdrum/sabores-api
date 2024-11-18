5665ff58369ad00deefc9cc85fe7bff72a59ab17
urpdrum <rm357140@fiap.com.br>
2024-11-18 12:17:22 -0300

    Atualizando aplication propiertes local

--- target/generated-sources/annotations/guia/saboresapi/domain/mapper/reserva/ReservaMapperImpl.java
+++ target/generated-sources/annotations/guia/saboresapi/domain/mapper/reserva/ReservaMapperImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-11-18T11:57:59-0300",
+    date = "2024-11-18T12:14:36-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.11 (Oracle Corporation)"
 )
 @Component
